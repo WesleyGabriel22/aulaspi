01702aaa38d0aeafbb96280799d3f9d85911d9ec
Wesley Gabriel Barros da Cunha <20211204010038@ifrn.local>
2024-10-15 10:04:08 -0300

    Criando a página de formulário para cadastro do evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EventosController {
-	@RequestMapping
+	
+	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
 	}
-	
+
 }
