0a0c1b3fcab8f760d5be08775da94f52b95609a6
Wesley Gabriel Barros da Cunha <20211204010038@ifrn.local>
2024-10-30 14:01:21 -0300

    Eviando dados do formulário para a aplicação

--- eventos-20241030T162132Z-001/eventos/src/main/java/ifrn/pi/eventos/models/Evento.java
+++ eventos-20241030T162132Z-001/eventos/src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -1,28 +1,10 @@
 package ifrn.pi.eventos.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
 public class Evento {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	private String nome;
 	private String local;
 	private String data;
 	private String horario;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public String getNome() {
 		return nome;
@@ -58,7 +40,7 @@ public class Evento {
 
 	@Override
 	public String toString() {
-		return "Evento[ID = " + id + ", Nome = " + nome + ", Local = " + local + ", Data = " + data + ", Horario = " + horario + "]";
+		return "Evento [Nome = " + nome + ", Local = " + local + ", Data = " + data + ", Horario = " + horario + "]";
 	}
 
 }
